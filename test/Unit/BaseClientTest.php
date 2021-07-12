4e9ce96fbc866ac3855a6d8e021e23d817002b9f
George Steel <george@net-glue.co.uk>
2021-07-12 12:03:17 +0100

    Initial rev of unit test for the client

---
+++ test/Unit/BaseClientTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GSteel\Listless\Octopus\Test\Unit;
+
+use GSteel\Listless\Octopus\BaseClient;
+use GSteel\Listless\Octopus\Test\Unit\Stub\CaseSensitiveEmail;
+use GSteel\Listless\Value\EmailAddress;
+use Laminas\Diactoros\RequestFactory;
+use Laminas\Diactoros\StreamFactory;
+use Laminas\Diactoros\UriFactory;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Client\ClientInterface;
+
+use function md5;
+
+class BaseClientTest extends TestCase
+{
+    /** @var BaseClient */
+    private $client;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->client = new BaseClient(
+            'AnyKey',
+            $this->createMock(ClientInterface::class),
+            new RequestFactory(),
+            new UriFactory(),
+            new StreamFactory(),
+            'http://0.0.0.0'
+        );
+    }
+
+    public function testThatMemberIdIsTheExpectedValue(): void
+    {
+        $expect = md5('me@example.com');
+        self::assertEquals($expect, $this->client->memberIdFromEmailAddress(EmailAddress::fromString('me@example.com')));
+    }
+
+    public function testThatTheMemberIdWillBeConsistentRegardlessOfEmailAddressCase(): void
+    {
+        $email = new CaseSensitiveEmail('ME@EXAMPLE.COM');
+        $expect = md5('me@example.com');
+        self::assertNotEquals($expect, md5($email->toString()));
+        self::assertEquals($expect, $this->client->memberIdFromEmailAddress($email));
+    }
+}
---
+++ test/Unit/Stub/CaseSensitiveEmail.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GSteel\Listless\Octopus\Test\Unit\Stub;
+
+use GSteel\Listless\Assert;
+use GSteel\Listless\EmailAddress;
+
+use function strtolower;
+
+/**
+ * @psalm-immutable
+ */
+final class CaseSensitiveEmail implements EmailAddress
+{
+    /** @var string */
+    private $email;
+
+    public function __construct(string $email)
+    {
+        Assert::email($email);
+        $this->email = $email;
+    }
+
+    public function __toString(): string
+    {
+        return $this->email;
+    }
+
+    public function toString(): string
+    {
+        return $this->email;
+    }
+
+    public function displayName(): ?string
+    {
+        return null;
+    }
+
+    public function isEqualTo(EmailAddress $other): bool
+    {
+        return strtolower($this->email) === strtolower($other->toString());
+    }
+}
